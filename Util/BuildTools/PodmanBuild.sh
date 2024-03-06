d41b9b869a079d1220a28749182592916789fd06
AreopagX <49414432+AreopagX@users.noreply.github.com>
2024-03-06 14:17:20 +0100

    edit build script

--- Util/BuildTools/PodmanBuild.sh
+++ Util/BuildTools/PodmanBuild.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -x
-podman run -t --rm --network host -v /home/carla/UE4.26:/home/carla/UE4.26 -v /home/carla/carla/:/home/carla/carla -u 1000:1000 --userns=keep-id ild-carla /bin/bash -c "cd /home/carla/carla && make package ARGS='--no-zip'"
+podman run -ti --rm --network host -v /home/carla/UE4.26:/home/carla/UE4.26 -v /home/carla/carla/:/home/carla/carla -u 1000:1000 --userns=keep-id ild-carla /bin/bash -c "${1}"
